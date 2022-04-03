82a9b070dc047109ad816dd83d9ad2ec464d7e7e
tr-asimpson <allen.simpson@tillerrides.com>
2022-04-03 14:20:31 +0800

    Fixed multiple request

--- packages/location/android/src/main/java/com/lyokone/location/FlutterLocation.java
+++ packages/location/android/src/main/java/com/lyokone/location/FlutterLocation.java
@@ -344,8 +344,36 @@ public class FlutterLocation
             result.success(1);
             return;
         }
-        ActivityCompat.requestPermissions(activity, new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
-                REQUEST_PERMISSIONS_REQUEST_CODE);
+
+        ActivityResultLauncher<String[]> locationPermissionRequest =
+                    registerForActivityResult(new ActivityResultContracts
+                        .RequestMultiplePermissions(), result -> {
+                            Boolean fineLocationGranted = result.getOrDefault(
+                                    Manifest.permission.ACCESS_FINE_LOCATION, false);
+                            Boolean coarseLocationGranted = result.getOrDefault(
+                                    Manifest.permission.ACCESS_COARSE_LOCATION,false);
+                            if (fineLocationGranted != null && fineLocationGranted) {
+                                // Precise location access granted.
+                            } else if (coarseLocationGranted != null && coarseLocationGranted) {
+                                // Only approximate location access granted.
+                            } else {
+                                // No location access granted.
+                            }
+                        }
+                    );
+
+                // ...
+
+                // Before you perform the actual permission request, check whether your app
+                // already has the permissions, and whether your app needs to show a permission
+                // rationale dialog. For more details, see Request permissions.
+                locationPermissionRequest.launch(new String[] {
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                });
+
+        // ActivityCompat.requestPermissions(activity, new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+        //         REQUEST_PERMISSIONS_REQUEST_CODE);
     }
 
     public boolean shouldShowRequestPermissionRationale() {
